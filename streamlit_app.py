ebd4cf0cd52f16ab0fb5e8d98af42b968fdf1b0e
NinhGiang <66211293+NinhGiang@users.noreply.github.com>
2021-06-23 21:40:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,80 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
 
-"""
-# Welcome to Streamlit!
+# add a title
+st.title('My first app')
+# write a dataframe
+st.write("Here's our first attempt at using data to create a table:")
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+st.write(df)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# draw a line chart
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.line_chart(chart_data)
+# plot a map
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.map(map_data)
 
+# Use checkboxes to show/hide data
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    chart_data
+# Use a selectbox for options
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+'You selected: ', option
 
-    points_per_turn = total_points / num_turns
+# beta_columns
+left_column, right_column = st.beta_columns(2)
+pressed = left_column.button('Press me?')
+if pressed:
+    right_column.write("Woohoo!")
+    
+# beta_expander
+expander = st.beta_expander("FAQ")
+expander.write("Here you could put in some really, really long explanations...")
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# markdown
+code = '''def hello():
+print("Hello, Streamlit!")'''
+st.code(code, language='python')
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# slider
+from datetime import datetime
+start_time = st.slider(
+    "When do you start?",
+    value=datetime(2020, 1, 1, 9, 30),
+    format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
+
+import time
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
